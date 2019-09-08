97de6adb6371e572fd1b0c49694d9113dfa8e4db
Rushit Saliya <rushitsaliya99@gmail.com>
2019-09-08 10:28:01 +0530

    Added databse password parameter
    password is "@root"

--- src/java/Database/DatabaseConnection.java
+++ src/java/Database/DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
     public Connection getConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/flight-management-and-booking", "root", "");
+            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/flight-management-and-booking", "root", "@root");
             return con;
         } catch(ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
